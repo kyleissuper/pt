d21df1bf6d78b8a9c1d743ed6c02a54d8bd6c268
Kyle Tan <kyle@kyletan.com>
2018-10-14 21:45:07 +0100

    Describer works, though ugly

--- static/script.js
+++ static/script.js
@@ -2,39 +2,59 @@ const coseLayout = require("./cytoscape-cose-bilkent.js");
 cytoscape.use(coseLayout);
 
 
+const describe = (evt) => {
+  let tbl = document.querySelector("#describer");
+  tbl.innerHTML = "";
+  let data = evt.target._private.data;
+  delete data.id;
+  Object.keys(data).forEach((k) => {
+    let row = "<tr><td>" + k + "</td>";
+    row += "<td>" + data[k] + "</td></tr>";
+    tbl.innerHTML += row;
+  });
+};
+
+
 var cy = window.cy = cytoscape({
   container: document.getElementById('cy'),
-
   boxSelectionEnabled: false,
   autounselectify: true,
-
   layout: {
     name: 'cose-bilkent',
     animate: false
   },
-
   style: [
     {
       selector: 'node',
       style: {
-        'background-color': '#ad1a66',
         'label': 'data(label)',
-        'font-size': '14px'
+        'background-color': '#000',
+        'color': '#fff',
+        'text-valign': 'center',
+        'text-outline-width': 2,
+        'text-outline-color': '#000',
+        'font-size': '10px'
       }
     },
-
     {
       selector: 'edge',
       style: {
-        'width': 3,
+        'width': 2,
+        'curve-style': 'bezier',
         'target-arrow-shape': 'triangle',
-        'line-color': '#ad1a66',
+        'line-color': '#000',
+        'target-arrow-color': '#000',
 				'label': 'data(label)',
-        'color': '#c00',
+        'color': 'hsl(0, 0%, 53.3%)',
         'font-size': '8px'
       }
     }
   ],
-
   elements: ontology
+});
+cy.on('tap', 'node', function(evt){
+  describe(evt);
+});
+cy.on('mouseover', 'node', function(evt){
+  describe(evt);
 });
